5ed343dada9111407dda61643493a4d18e37dae2
Banderarius <2.vanya.k@gmail.com>
2022-06-02 03:23:00 +0300

    Configured endpoints for CRUD operations on users

--- API/Api.cs
+++ API/Api.cs
@@ -8,7 +8,10 @@ public static class Api
     public static void ConfigureApi(this WebApplication app)
     {
         app.MapGet("/users", GetUsers);
+        app.MapGet("users/%id", GetUserById);
         app.MapPost("/users", InsertUser);
+        app.MapPut("/users", UpdateUser);
+        app.MapDelete("users/%id", DeleteUser);
     }
 
     private static async Task<IResult> GetUsers(IUserRepository userRepo)
@@ -29,6 +32,47 @@ public static class Api
         try
         {
             await userRepo.Insert(user);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> GetUserById(IUserRepository userRepo, int id)
+    {
+        try
+        {
+            return Results.Ok(await userRepo.Get(id));
+        }
+        catch (Exception ex)
+        {
+
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> DeleteUser(IUserRepository userRepo, int id)
+    {
+        try
+        {
+            await userRepo.Delete(id);
+            return Results.Ok();
+        }
+        catch (Exception ex)
+        {
+
+            return Results.Problem(ex.Message);
+        }
+    }
+
+    private static async Task<IResult> UpdateUser(IUserRepository userRepo, User user)
+    {
+        try
+        {
+            await userRepo.Update(user);
             return Results.Ok();
         }
         catch (Exception ex)
