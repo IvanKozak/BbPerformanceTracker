02aa668927db735b176fbc03ec1c5f3c210bcd1c
Banderarius <2.vanya.k@gmail.com>
2022-07-17 16:35:30 +0300

    Adding loading user from authentication

---
+++ BlazorApp/Helpers/AuthenticationStateProviderHelpers.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary.DataRepositories;
+using ClassLibrary.Models;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorApp.Helpers;
+
+public static class AuthenticationStateProviderHelpers
+{
+    public static async Task<User> GetUserFromAuth(this AuthenticationStateProvider authProvider, IUserRepository userRepo)
+    {
+        var authState = await authProvider.GetAuthenticationStateAsync();
+        var objectId = authState.User.Claims.FirstOrDefault(c => c.Type.Contains("objectidentifier"))?.Value;
+        return await userRepo.GetFromAuthentication(objectId);
+    }
+}
