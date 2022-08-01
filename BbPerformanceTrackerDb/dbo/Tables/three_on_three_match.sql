3e21c2d8c398ed1b4e14c9a7833608431e57991c
Banderarius <2.vanya.k@gmail.com>
2022-08-01 18:32:40 +0300

    Added and renamed columns in 3x3match table.

--- BbPerformanceTrackerDb/dbo/Tables/three_on_three_match.sql
+++ BbPerformanceTrackerDb/dbo/Tables/three_on_three_match.sql
@@ -1,12 +1,16 @@
 ﻿CREATE TABLE [dbo].[3x3_match]
 (
 	[id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [user_id] INT NOT NULL FOREIGN KEY REFERENCES [user](Id) ON DELETE CASCADE, 
+    [user_id] INT NOT NULL FOREIGN KEY REFERENCES [user](id) ON DELETE CASCADE, 
     [accomplished] DATETIME NOT NULL, 
+    [own_team_score] INT NOT NULL,
+    [other_team_score] INT NOT NULL,
+    [one_point_makes] INT NOT NULL, 
+    [one_point_attempts] INT NOT NULL,
     [two_point_makes] INT NOT NULL, 
     [two_point_attempts] INT NOT NULL, 
-    [one_point_makes] INT NOT NULL, 
-    [one_point_attempts] INT NOT NULL, 
-    [assists] INT NOT NULL, 
-    [rebounds] INT NOT NULL
+    [free_throw_makes] INT NOT NULL, 
+    [free_throw_attempts] INT NOT NULL, 
+    [rebounds] INT NOT NULL,
+    [assists] INT NOT NULL  
 )
