2167413410cd99fe25e2a779f7438e0c7a8cb512
Banderarius <2.vanya.k@gmail.com>
2022-08-03 18:23:54 +0300

    Add extention method for calculating effective field goal percentage.

---
+++ BlazorApp/Helpers/ThreeOnThreeMatchExtentions.cs
@@ -0,0 +1,11 @@
+﻿using ClassLibrary.Models;
+
+namespace BlazorApp.Helpers;
+
+public static class ThreeOnThreeMatchExtentions
+{
+    public static decimal EffectiveFieldGoalPercentage(this ThreeOnThreeMatch match)
+    {
+        return (match.OwnPoints - match.FreeThrows.Makes) * 100 / (match.OnePointShots.Attempts + match.TwoPointShots.Attempts);
+    }
+}
