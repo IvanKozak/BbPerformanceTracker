184dac9c4a7a0b1f7051c15cc391d4644d9ca9d9
Ivan Kozak <2.vanya.k@gmail.com>
2024-01-24 16:25:26 +0100

    Add Stored Procedure
    Added procedure for getting Shooting Drills by B2C Identifier.

---
+++ BbPerformanceTrackerDb/dbo/StoredProcedures/spShootingDrill_GetAllByB2CId.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spShootingDrill_GetAllByB2CId]
+	@B2CIdentifier nvarchar(50)
+
+AS
+
+    BEGIN
+	    SELECT  shooting_drill.id AS Id, user_id AS UserId,
+                [user].nickname AS UserNickname, [user].b2c_identifier AS UserB2CIdentifier,
+                accomplished, three_pointer_makes AS ThreePointerMakes, three_pointer_attempts AS ThreePointerAttempts,
+                midrange_makes AS MidrangeMakes, midrange_attempts AS MidrangeAttempts,
+                postup_makes AS PostupMakes, postup_attempts AS PostupAttempts
+        FROM shooting_drill INNER JOIN [user] ON shooting_drill.user_id=[user].id
+        WHERE [user].b2c_identifier = @B2CIdentifier
+    END
